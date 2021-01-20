6ea2c0ff62d13f30e41fc5d4fe395be850dda781
Chayan Banerjee <chayan2003@gmail.com>
2021-01-20 15:43:56 +0530

    added validations and refactored GetFundOfMandates

--- Rabobank.Intake/Rabobank.Intake.Library/FundOfMandateCalculator.cs
+++ Rabobank.Intake/Rabobank.Intake.Library/FundOfMandateCalculator.cs
@@ -7,30 +7,41 @@
     using System.Xml.Serialization;
     using Rabobank.Intake.Library.Interfaces;
     using Rabobank.Intake.Library.Model;
+    using Rabobank.Intake.Library.Resources;
 
     public class FundOfMandateCalculator : IFundOfMandateCalculator
     {
-        public FundsOfMandatesData GetFundOfMandates(string fileName)
+        /// <summary>
+        /// List of validators to validate file
+        /// </summary>
+        private static readonly List<(Func<string, bool> validate, string message)> _fileValidators;
+
+        static FundOfMandateCalculator()
         {
-            if (fileName == null)
+            _fileValidators = new List<(Func<string, bool> validate, string message)>
             {
-                return null;
-            }
+                (FilePathProvided, ErrorMessages.FilePathNotProvided),
+                (FileExists, ErrorMessages.FileDoesNotExist),
+                (IsXmlFile, ErrorMessages.NotXmlFile)
+            };
+        }
 
-            List<FundOfMandates> listFundOfMandates = new List<FundOfMandates>();
-
-            XmlSerializer serializer = new XmlSerializer(typeof(FundsOfMandatesData));
-            using (TextReader reader = new StringReader(File.ReadAllText(fileName)))
+        /// <summary>
+        /// Reads provided file to build fundOfMandatesData
+        /// </summary>
+        /// <param name="mandatesFileName">xml file from where we can read the fundOfMandatesData</param>
+        /// <returns></returns>
+        public FundsOfMandatesData GetFundOfMandates(string mandatesFileName)
+        {
+            _fileValidators.ForEach(validator =>
             {
-                FundsOfMandatesData result = (FundsOfMandatesData)serializer.Deserialize(reader);
+                if (!validator.validate(mandatesFileName))
+                    throw new ArgumentException(validator.message);
+            });
 
-                foreach (var item in result.FundsOfMandates)
-                {
-                    listFundOfMandates.Add(item);
-                }
-            }
-
-            return new FundsOfMandatesData { FundsOfMandates = listFundOfMandates.ToArray() };
+            using var stream = new FileStream(mandatesFileName, FileMode.Open, FileAccess.Read);
+            var serializer = new XmlSerializer(typeof(FundsOfMandatesData));
+            return (FundsOfMandatesData)serializer.Deserialize(stream);
         }
 
         public Portfolio CalculateMandates(Portfolio portfolio, FundsOfMandatesData fundOfMandatesData)
@@ -72,5 +83,30 @@
 
             return portfolio;
         }
+
+        #region mandate file validation
+
+        /// <summary>
+        /// validate that the file path is provided 
+        /// </summary>
+        /// <param name="filePath">file path from where the mandates can be read</param>
+        /// <returns>True if file path is provided</returns>
+        private static bool FilePathProvided(string filePath) => !string.IsNullOrEmpty(filePath);
+
+        /// <summary>
+        /// validates that the file exists at the provided location
+        /// </summary>
+        /// <param name="filePath">file path from where the mandates can be read</param>
+        /// <returns>True if the file exists at provided location</returns>
+        private static bool FileExists(string filePath) => FilePathProvided(filePath) && File.Exists(filePath);
+
+        /// <summary>
+        /// Validates that the provided file is an xml file
+        /// </summary>
+        /// <param name="filePath">file path from where the mandates can be read</param>
+        /// <returns>True if file is xml file</returns>
+        private static bool IsXmlFile(string filePath) => FilePathProvided(filePath) && Path.GetExtension(filePath).Equals(".xml", StringComparison.OrdinalIgnoreCase);
+
+        #endregion mandate file validation
     }
 }
